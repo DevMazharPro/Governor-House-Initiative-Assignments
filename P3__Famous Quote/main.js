e0165352fe8f1d01bdcc2adf92ae04b211dff18b
DevMazharPro <ibrarhalepotooo@gmail.com>
2024-02-19 23:18:51 +0500

    // Third

---
+++ P3__Famous Quote/main.js
@@ -0,0 +1,3 @@
+var quote = "The only thing we have to fear is fear itself.";
+var author = "Franklin D. Roosevelt";
+console.log("".concat(author, " once said, \"").concat(quote, "\""));
---
+++ P3__Famous Quote/main.ts
@@ -0,0 +1,4 @@
+let quote = "The only thing we have to fear is fear itself.";
+let author = "Franklin D. Roosevelt";
+
+console.log(`${author} once said, "${quote}"`);
