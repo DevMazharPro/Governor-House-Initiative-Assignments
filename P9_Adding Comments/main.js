06993ae3028b0c2938f860c92822c0e273b04c4b
DevMazharPro <ibrarhalepotooo@gmail.com>
2024-02-21 19:11:06 +0500

    // 9th

---
+++ P9_Adding Comments/main.js
@@ -0,0 +1,10 @@
+// This program adds my first and last name, then tells my full name.
+var firstName = "Mazhar Hussain";
+var lastName = " Halepoto";
+var fullName = firstName + lastName;
+console.log("Hi, My full name is ".concat(fullName, "."));
+// This program sums up 2 numbers and shows result.
+var numb1 = 5;
+var numb2 = 7;
+var sumResult = numb1 + numb2;
+console.log("If you add ".concat(numb1, " and ").concat(numb2, ", you will get ").concat(sumResult, "."));
---
+++ P9_Adding Comments/main.ts
@@ -0,0 +1,19 @@
+// This program adds my first and last name, then tells my full name.
+
+let firstName: string = "Mazhar Hussain";
+let lastName: string = " Halepoto";
+
+let fullName: string = firstName + lastName;
+console.log(`Hi, My full name is ${fullName}.`);
+
+// This program sums up 2 numbers and shows result.
+
+let numb1: number = 5;
+let numb2: number = 7;
+
+let sumResult: number = numb1 + numb2;
+console.log(`If you add ${numb1} and ${numb2}, you will get ${sumResult}.`);
+
+
+ 
+
