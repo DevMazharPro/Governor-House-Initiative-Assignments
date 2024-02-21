506ac953fbb876185d2a43b5e6918fb15c6efa97
DevMazharPro <ibrarhalepotooo@gmail.com>
2024-02-21 20:30:56 +0500

    //16th question

---
+++ P14_Update to Guest list/main.ts
@@ -0,0 +1,24 @@
+// Question 14
+var guestNames = ["Ali", "Elon Musk", "Daniyal"];
+guestNames.forEach(function (guests) {
+    console.log("Hey ".concat(guests, ", you are invited for Dinner at Home."));
+});
+// 15Question Starting
+var guestUnavailable = "Ali";
+var newGuest = "Malik";
+guestNames[0] = newGuest;
+guestNames.forEach(function (guests) {
+    console.log("Hey ".concat(guests, ", you are invited for Dinner at Home."));
+});
+console.log("".concat(guestNames[0], ", is not available today."));
+
+
+// 16Question Starting
+
+console.log("Great news, we got a bigger table. We have invite three more friends");
+guestNames.unshift("Falak");
+guestNames.splice(Math.ceil(guestNames.length/2), 0, "Azhar");
+guestNames.push("Masroor");
+guestNames.forEach((guests:string)=>{
+    console.log(`Hey ${guests}, you are invited for Dinner at Home.`);
+})
