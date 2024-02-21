f3577ca837ce51b25d5bbb44c4ddcdb2a33bb417
DevMazharPro <ibrarhalepotooo@gmail.com>
2024-02-21 18:36:43 +0500

    /fifth

---
+++ P5_Stripping Names/main.js
@@ -0,0 +1,6 @@
+var personName = "\t   John Doe\n";
+console.log("Name along with whitespace:");
+console.log(personName);
+var cleanedName = personName.trim();
+console.log("\nName after stripping whitespaces:");
+console.log(cleanedName);
---
+++ P5_Stripping Names/main.ts
@@ -0,0 +1,8 @@
+let personName: string = "\t   John Doe\n";
+console.log("Name along with whitespace:");
+console.log(personName);
+
+
+let cleanedName: string = personName.trim();
+console.log("\nName after stripping whitespaces:");
+console.log(cleanedName);
