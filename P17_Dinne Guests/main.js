63ef56197fca0dc8fb07e1fc756a58c7dde7b009
DevMazharPro <ibrarhalepotooo@gmail.com>
2024-02-21 22:49:30 +0500

    //19th

---
+++ P17_Dinne Guests/main.js
@@ -0,0 +1,3 @@
+var invitedFriends = ["Falak", "Malik"];
+var invitationCount = invitedFriends.length;
+console.log("The number of friends invited at dinner is ".concat(invitationCount, "."));
---
+++ P17_Dinne Guests/main.ts
@@ -0,0 +1,4 @@
+let invitedFriends: string[]= ["Falak", "Malik"];
+let invitationCount: Number= invitedFriends.length;
+console.log(`The number of friends invited at dinner is ${invitationCount}.`);
+
