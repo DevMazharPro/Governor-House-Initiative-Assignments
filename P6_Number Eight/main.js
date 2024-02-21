0e2f1d686866e7d4d555ba5f250f5d8cbc036fb7
DevMazharPro <ibrarhalepotooo@gmail.com>
2024-02-21 18:44:40 +0500

    // 6th\

---
+++ P6_Number Eight/main.js
@@ -0,0 +1,8 @@
+var additionResults = 5 + 3;
+console.log("Addition ".concat(additionResults));
+var subtactionResults = 10 - 2;
+console.log("Subrtaction ".concat(subtactionResults));
+var multiplicationResults = 2 * 4;
+console.log("Multiplication ".concat(multiplicationResults));
+var divisionResults = 16 / 2;
+console.log("Division ".concat(divisionResults));
---
+++ P6_Number Eight/main.ts
@@ -0,0 +1,11 @@
+let additionResults: number = 5 + 3;
+console.log(`Addition ${additionResults}`);
+
+let subtactionResults: number = 10 - 2;
+console.log(`Subrtaction ${subtactionResults}`);
+
+let multiplicationResults: number = 2 * 4;
+console.log(`Multiplication ${multiplicationResults}`);
+
+let divisionResults: number = 16 / 2;
+console.log(`Division ${divisionResults}`);
