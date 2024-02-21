614a62fe4ed6d7c37750f17c0a79f0d91c55ef7e
DevMazharPro <ibrarhalepotooo@gmail.com>
2024-02-21 18:30:23 +0500

    // fourth

---
+++ P4_Famous Quote2/main.js
@@ -0,0 +1,3 @@
+var famousPerson = "Heraclitus";
+var massage = "There is nothing permanent except change.";
+console.log("".concat(famousPerson, " once said, \"").concat(massage, "\""));
---
+++ P4_Famous Quote2/main.ts
@@ -0,0 +1,3 @@
+let famousPerson: string= "Heraclitus";
+let massage: string= "There is nothing permanent except change.";
+console.log(`${famousPerson} once said, "${massage}"`);
