3c1bbcc4620e3b0cc32163f656f506ef79b5c03c
DevMazharPro <ibrarhalepotooo@gmail.com>
2024-02-21 19:24:13 +0500

    // 11th

---
+++ P10_Greetings/main.js
@@ -0,0 +1,4 @@
+var friends = ["Fahad", "Israr", "Manik", "Amna", "Maryam", "Haziq"];
+friends.forEach(function (names) {
+    console.log("Hello ".concat(names, ", How are you?"));
+});
---
+++ P10_Greetings/main.ts
@@ -0,0 +1,4 @@
+let friends: string[]= ["Fahad", "Israr", "Manik", "Amna", "Maryam", "Haziq"];
+friends.forEach ((names:string)=>{
+    console.log(`Hello ${names}, How are you?`);  
+})
