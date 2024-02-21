e2db61ec81320f96057859c19e94d0166ffc9468
DevMazharPro <ibrarhalepotooo@gmail.com>
2024-02-21 19:42:23 +0500

    // 13th

---
+++ P12_Guest Names/main.js
@@ -0,0 +1,4 @@
+var guestNames = ["Ali", "Elon Musk", "Daniyal"];
+guestNames.forEach(function (guests) {
+    console.log("Hey ".concat(guests, ", you are invited for Dinner at Home."));
+});
---
+++ P12_Guest Names/main.ts
@@ -0,0 +1,4 @@
+let guestNames: string[]= ["Ali", "Elon Musk", "Daniyal"];
+guestNames.forEach((guests:string)=>{
+    console.log(`Hey ${guests}, you are invited for Dinner at Home.`);
+})
