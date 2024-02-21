e271464d4585ddb89ab2d27ac7616755ed1bd4ee
DevMazharPro <ibrarhalepotooo@gmail.com>
2024-02-21 19:35:09 +0500

    // 12th

---
+++ P11_Transport Choices/main.js
@@ -0,0 +1,4 @@
+var transportChoices = ["Bus", "Car", "Bike", "Aeroplane"];
+transportChoices.forEach(function (choice) {
+    console.log("\"I would like to ride by ".concat(choice, ".\u201D"));
+});
---
+++ P11_Transport Choices/main.ts
@@ -0,0 +1,4 @@
+let transportChoices: string[]= ["Bus", "Car", "Bike", "Aeroplane"]
+transportChoices.forEach((choice:string)=>{
+    console.log(`"I would like to ride by ${choice}.”`);
+})
