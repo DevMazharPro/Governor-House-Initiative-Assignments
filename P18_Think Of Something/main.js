5c213f5e0da160655b89c08a0c1bf5de86c14624
DevMazharPro <ibrarhalepotooo@gmail.com>
2024-02-21 23:02:37 +0500

    //20th

---
+++ P18_Think Of Something/main.js
@@ -0,0 +1,3 @@
+var mobileBrands = ["Apple", "Vivo", "Oppo", "Samsung", "Redmi", "Tecno", "Realme"];
+console.log("List of Current Pakistani Mobile Brands:");
+console.log(mobileBrands);
---
+++ P18_Think Of Something/main.ts
@@ -0,0 +1,3 @@
+let mobileBrands: string[]= ["Apple", "Vivo", "Oppo", "Samsung", "Redmi", "Tecno", "Realme"];
+console.log("List of Current Pakistani Mobile Brands:");
+console.log(mobileBrands);
