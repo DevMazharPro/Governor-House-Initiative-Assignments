1d4e5df1eef3442a0c3dfe563e7853700bd9076e
DevMazharPro <ibrarhalepotooo@gmail.com>
2024-02-22 19:25:34 +0500

    //28th

---
+++ P26_Stages Of Life/main.js
@@ -0,0 +1,25 @@
+var humanAge = 22;
+if (humanAge < 2) {
+    console.log("Person is a baby.");
+}
+else if (humanAge < 2) {
+    console.log("Person is a baby.");
+}
+else if (humanAge >= 2 && humanAge < 4) {
+    console.log("Person is a toddler.");
+}
+else if (humanAge >= 4 && humanAge < 13) {
+    console.log("Person is a Kid.");
+}
+else if (humanAge >= 13 && humanAge < 20) {
+    console.log("Person is a Teenager.");
+}
+else if (humanAge >= 20 && humanAge < 65) {
+    console.log("Person is a Adult.");
+}
+else if (humanAge >= 65) {
+    console.log("Person is a Elder.");
+}
+else {
+    console.log("Person is nowadays Ghost!");
+}
