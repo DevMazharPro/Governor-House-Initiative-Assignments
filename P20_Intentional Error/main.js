b5c2f9a09ccf54b683da380c55d3de10d1524d7f
DevMazharPro <ibrarhalepotooo@gmail.com>
2024-02-22 17:31:59 +0500

    // 22nd

---
+++ P20_Intentional Error/main.js
@@ -0,0 +1,5 @@
+var carModels = ["Toyota", "Honda", "Suzuki", "BMW", "Audi"];
+//Intentional Error
+console.log(carModels[6]);
+// Error Fixed
+console.log(carModels[4]);
---
+++ P20_Intentional Error/main.ts
@@ -0,0 +1,6 @@
+let carModels: string[]= ["Toyota", "Honda", "Suzuki", "BMW", "Audi"]
+//Intentional Error: Accessing a string which is not available
+console.log(carModels[6]);
+// Error Fixed
+console.log(carModels[4]);
+
