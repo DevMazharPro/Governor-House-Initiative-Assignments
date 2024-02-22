96fcc626a4576f66d2efcdb853c3fd5c41c026b3
DevMazharPro <ibrarhalepotooo@gmail.com>
2024-02-22 19:39:06 +0500

    // 29th

---
+++ P27_Favourite Fruits/main.js
@@ -0,0 +1,16 @@
+var favouriteFruits = ["Mango", "Grapes", "Strawberry"];
+if (favouriteFruits.includes("Mango")) {
+    console.log("You really like Mangoes!");
+}
+if (favouriteFruits.includes("Grapes")) {
+    console.log("You really like Grapes!");
+}
+if (favouriteFruits.includes("Strawberry")) {
+    console.log("You really like Strawberries!");
+}
+if (favouriteFruits.includes("Bananas")) {
+    console.log("You really like Bananas!");
+}
+if (favouriteFruits.includes("Pineapple")) {
+    console.log("You really like Pineapple!");
+}
