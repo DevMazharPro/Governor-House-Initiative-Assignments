d1377b3ad28710391f64f5cb271930452303f9ef
DevMazharPro <ibrarhalepotooo@gmail.com>
2024-02-22 18:49:59 +0500

    //25th

--- P23_Alien Colors/main.ts
+++ P23_Alien Colors/main.ts
@@ -1,9 +1,7 @@
 let alienColors: string= "Yellow";
 if (alienColors = "Yellow"){
     console.log("Congratulations, you just earned 5 points.")
-} else {
-    console.log("You loose")
-}
+} 
 
 let alienColor1: string= "red";
 if (alienColor1 == "yellow"){
