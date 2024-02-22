ab2af333a2f4788b09c47ed4c664d89ff4cca20d
DevMazharPro <ibrarhalepotooo@gmail.com>
2024-02-22 18:00:29 +0500

    //23rd

---
+++ P21_Conditional Tests/main.js
@@ -0,0 +1,23 @@
+var age = 18;
+// Is the age exactly 18? I predict True.
+console.log(age == 18);
+// Age is eligible for under 19 World Cup? I predict True.
+console.log(age < 19);
+// Is there a requirement to be 18 years old for CNIC? I predict True.
+console.log(age >= 18);
+// We receive Driving license before being 18 years old? I predict False.
+console.log(age == 17);
+// Is the age is greater than or equal to 20? I predict false
+console.log(age >= 20);
+var color = "yellow";
+// Sunflower has a Yellowish Color? I predict True.
+console.log(color === "yellow");
+// Watermelon has a yellow color? I predict False.
+console.log(color === "red");
+// You love Yellow Color? I predict True.
+console.log(color != "Yellow");
+var weekend = false;
+// Is weekends are business days? I predict false
+console.log(weekend);
+// Is weekends are Working days? I predict false
+console.log(weekend);
