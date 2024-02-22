66f3a00e8f7523982880cea6ff2afaff7b612b1f
DevMazharPro <ibrarhalepotooo@gmail.com>
2024-02-22 19:04:42 +0500

    // 26th

---
+++ P24_Alien Colors #2/main.js
@@ -0,0 +1,16 @@
+// If block runs
+var alienColor = "green";
+if (alienColor === "green") {
+    console.log("Congrats! You just earned 5 points for shooting an green Alien.");
+}
+else {
+    console.log("Congrats! You just earned 10 points for shooting the red Alien.");
+}
+// Else block runs
+var alienColor2 = "red";
+if (alienColor2 === "green") {
+    console.log("Congrats! You just earned 5 points for shooting an Alien.");
+}
+else {
+    console.log("Congrats! You earned 10 points for shooting red Alien.");
+}
---
+++ P24_Alien Colors #2/main.ts
@@ -0,0 +1,15 @@
+// If block runs
+let alienColor: string = "green";
+if (alienColor === "green"){
+    console.log("Congrats! You just earned 5 points for shooting an green Alien.");
+} else {
+    console.log("Congrats! You just earned 10 points for shooting the red Alien.");
+}
+// Else block runs
+let alienColor2: string = "red";
+if (alienColor2 === "green"){
+    console.log("Congrats! You just earned 5 points for shooting an Alien.")
+} else {
+    console.log("Congrats! You earned 10 points for shooting red Alien.");
+
+}
