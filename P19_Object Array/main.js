e50cdabb7ace53ebacde6d606c49e9bb03251dab
DevMazharPro <ibrarhalepotooo@gmail.com>
2024-02-22 17:24:58 +0500

    //21st

--- P19_Object Array/main.js
+++ P19_Object Array/main.js
@@ -2,5 +2,6 @@
 var mobileBrands = ["Apple", "Vivo", "Oppo", "Samsung", "Redmi", "Tecno", "Realme"];
 console.log("List of Current Pakistani Mobile Brands:");
 console.log(mobileBrands);
+// 21Question
 var appleProduct = { name: "Iphone", model: 15, varient: "Pro Max" };
 console.log("Latest Apple Mobile", appleProduct);
--- P19_Object Array/main.ts
+++ P19_Object Array/main.ts
@@ -2,6 +2,6 @@
 let mobileBrands: string[]= ["Apple", "Vivo", "Oppo", "Samsung", "Redmi", "Tecno", "Realme"];
 console.log("List of Current Pakistani Mobile Brands:");
 console.log(mobileBrands);
-// 21Q
+// 21Question
 let appleProduct: {name: string, model:number, varient:string} = {name: "Iphone", model: 15, varient: "Pro Max"};
 console.log("Latest Apple Mobile", appleProduct)
