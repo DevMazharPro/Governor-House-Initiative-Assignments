1ae91a104aa8cf3a9e0fa5854626f8d297fa5048
DevMazharPro <ibrarhalepotooo@gmail.com>
2024-02-22 19:14:15 +0500

    // 27th

---
+++ P25_Alien Colors #3/main.js
@@ -0,0 +1,13 @@
+var alien = "green";
+if (alien == "green") {
+    console.log("Congrats! You just earned 5 points for shooting green Alien");
+}
+else if (alien === "yellow") {
+    console.log("Congrats! You just earned 10 points for shooting yellow Alien");
+}
+else if (alien === "red") {
+    console.log("Congrats! You just earned 15 points for shooting red Alien");
+}
+else {
+    console.log("No points, Sorry!");
+}
---
+++ P25_Alien Colors #3/main.ts
@@ -0,0 +1,13 @@
+let alien: string = "green";
+if (alien == "green"){
+    console.log("Congrats! You just earned 5 points for shooting green Alien");
+} 
+else if ( alien === "yellow"){
+    console.log("Congrats! You just earned 10 points for shooting yellow Alien");
+} 
+else if( alien === "red"){
+    console.log("Congrats! You just earned 15 points for shooting red Alien");
+} 
+else {
+    console.log("No points, Sorry!");
+}
