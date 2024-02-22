7cbd5ab56375a1478c3f83038bf995045a31493a
DevMazharPro <ibrarhalepotooo@gmail.com>
2024-02-22 18:45:28 +0500

    // 25th

---
+++ P23_Alien Colors/main.js
@@ -0,0 +1,11 @@
+var alienColors = "Yellow";
+if (alienColors = "Yellow") {
+    console.log("Congratulations, you just earned 5 points.");
+}
+else {
+    console.log("You loose");
+}
+var alienColor1 = "red";
+if (alienColor1 == "yellow") {
+    console.log("No Output");
+}
---
+++ P23_Alien Colors/main.ts
@@ -0,0 +1,11 @@
+let alienColors: string= "Yellow";
+if (alienColors = "Yellow"){
+    console.log("Congratulations, you just earned 5 points.")
+} else {
+    console.log("You loose")
+}
+
+let alienColor1: string= "red";
+if (alienColor1 == "yellow"){
+    console.log("No Output")
+}
