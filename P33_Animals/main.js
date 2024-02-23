496ff75c2b63812960e57c61306b02ccbb3e073f
DevMazharPro <ibrarhalepotooo@gmail.com>
2024-02-23 11:09:46 +0500

    //35th

---
+++ P33_Animals/main.js
@@ -0,0 +1,11 @@
+var animals = ["Dog", "Cat", "Goat"];
+console.log("List of animals:");
+for (var _i = 0, animals_1 = animals; _i < animals_1.length; _i++) {
+    var pets = animals_1[_i];
+    console.log(pets);
+}
+for (var _a = 0, animals_2 = animals; _a < animals_2.length; _a++) {
+    var pets = animals_2[_a];
+    console.log("A ".concat(pets, " would make a great pet."));
+}
+console.log("These are good pets.");
---
+++ P33_Animals/main.ts
@@ -0,0 +1,9 @@
+let animals: string[] = ["Dog", "Cat", "Goat"];
+console.log("List of animals:");
+for (let pets of animals){
+    console.log(pets);
+}
+for (let pets of animals){
+console.log(`A ${pets} would make a great pet.`);
+}
+console.log("These are good pets.")
