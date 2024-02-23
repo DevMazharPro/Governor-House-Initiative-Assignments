f6eca4bd30d775c6d6ef5673524cc28a1e6faced
DevMazharPro <ibrarhalepotooo@gmail.com>
2024-02-23 11:53:29 +0500

    // 41st

---
+++ P39_Magicians/main.js
@@ -0,0 +1,8 @@
+function showMagicians(names) {
+    for (var _i = 0, magicianNames_1 = magicianNames; _i < magicianNames_1.length; _i++) {
+        var magician = magicianNames_1[_i];
+        console.log(magician);
+    }
+}
+var magicianNames = ["Mazhar", "Jadu", "Maa", "Baap"];
+showMagicians(magicianNames);
---
+++ P39_Magicians/main.ts
@@ -0,0 +1,7 @@
+function showMagicians(names: string[]): void{
+    for (let magician of magicianNames){
+        console.log(magician);
+    }
+}
+let magicianNames: string[]= ["Mazhar", "Jadu", "Maa", "Baap"];
+showMagicians(magicianNames);
