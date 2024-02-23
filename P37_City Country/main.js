e81436c991e8ea43f8975078f69d7eadfda28851
DevMazharPro <ibrarhalepotooo@gmail.com>
2024-02-23 11:36:26 +0500

    //39th

---
+++ P37_City Country/main.js
@@ -0,0 +1,5 @@
+function cityCountry(city, country) {
+    return ("".concat(city, ",").concat(country));
+}
+var cC = cityCountry("Karachi", "Pakistan");
+console.log(cC);
---
+++ P37_City Country/main.ts
@@ -0,0 +1,5 @@
+function cityCountry(city: string, country: string): string {
+return(`${city},${country}`)
+}
+let cC: string= cityCountry("Karachi", "Pakistan");
+console.log(cC);
