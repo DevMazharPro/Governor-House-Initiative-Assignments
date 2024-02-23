b73668cce7037199f0e7316c70df033306c49618
DevMazharPro <ibrarhalepotooo@gmail.com>
2024-02-23 11:02:02 +0500

    // 34th

---
+++ P32_Favourite Pizza/main.js
@@ -0,0 +1,12 @@
+var favoritePizzas = ["Afghani", "Kababish", "BBQ Chicken"];
+console.log("My favorite pizzas:");
+for (var _i = 0, favoritePizzas_1 = favoritePizzas; _i < favoritePizzas_1.length; _i++) {
+    var pizza = favoritePizzas_1[_i];
+    console.log(pizza);
+}
+//other half
+for (var _a = 0, favoritePizzas_2 = favoritePizzas; _a < favoritePizzas_2.length; _a++) {
+    var pizza = favoritePizzas_2[_a];
+    console.log("I like ".concat(pizza, " pizza."));
+}
+console.log("I really love pizza!");
---
+++ P32_Favourite Pizza/main.ts
@@ -0,0 +1,14 @@
+let favoritePizzas: string[] = ["Afghani", "Kababish", "BBQ Chicken"];
+
+console.log("My favorite pizzas:");
+
+for (let pizza of favoritePizzas) {
+    console.log(pizza);
+}
+
+//other half
+for (let pizza of favoritePizzas) {
+    console.log(`I like ${pizza} pizza.`);
+}
+
+console.log("I really love pizza!");
